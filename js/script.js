00de881fb90593f01130ac717276738fc47cd497
andreacapntd <andreacapntd@gmail.com>
2020-07-22 15:52:34 +0200

    js working in progress

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
+/* Attraverso una chiamata ajax all'Api di boolean avremo a
+disposizione una decina di dischi musicali.
+Servendoci di handlebars stampiamo tutto a schermo.
+In questo momento non è importante la parte grafica. */
 
+function getDiscMusic() {
+
+  $.ajax({
+
+    url: 'https://flynn.boolean.careers/exercises/api/array/music',
+    method: 'GET',
+    success: function(data) {
+
+      var success = data['success'];
+      var values = data['response'];
+
+      for (var i = 0; i < values.length; i++) {
+
+        var cover = values[i]['poster'];
+        var cdTitle = values[i]['title'];
+        var artist = values[i]['author'];
+        var year = values[i]['year'];
+      }
+    },
+    error: function() {
+
+      console.log('error');
+    }
+  })
+}
 
 
 
@@ -23,7 +52,9 @@
 
 
 function init() {
-  
+
+  getDiscMusic();
+
 };
 
 
